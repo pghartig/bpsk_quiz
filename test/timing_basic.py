f1e650514280ca1d795a91a175c7bf80bd14bf4e
Peter Hartig <peterhartig@Peters-MBP-2.fios-router.home>
2022-04-30 19:34:48 -0400

    Adding timing recovery test

--- test/timing_basic.py
+++ test/timing_basic.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.signal import firwin
 import numpy as np
 
-num_bits = 50000
+num_bits = 100000
 alpha = .35
 Ts = 1/1e6
 fs_rx = 4e6
@@ -16,6 +16,7 @@ nrz_steam = -np.random.randint(0, 2, num_bits)*2 + 1
 BER = []
 rrc = rrcosfilter(4, alpha, Ts, fs_rx)[1][1:]
 rrc /= np.linalg.norm(rrc)
+delay_filter_length = 31
 
 for ebno_db in ebnos_db:
     ebno = np.power(10, ebno_db/10)
@@ -23,22 +24,29 @@ for ebno_db in ebnos_db:
     rx_signal = 1j*np.zeros(oversample*len(nrz_steam))
     # Use random doppler in ppm tolerance range
     ppm_error = np.random.uniform(-30, 30)/1e6
+    # timing_error = np.random.uniform(0, 4)
+    timing_error = 0
+    delay = np.sinc(np.arange(-delay_filter_length//2, delay_filter_length//2) - timing_error)
+    delay *= np.hamming(delay_filter_length)
+    delay /= np.sum(delay)
     doppler = f_c*ppm_error
     phase_offset = np.random.uniform(-np.pi, np.pi)
-    # Allowed to assume timing is preserved througout.
-    timing_offset = 0
-    rx_signal[timing_offset::oversample] = nrz_steam
+    symbol_offset = 0
+    rx_signal[symbol_offset::oversample] = nrz_steam
     rx_signal = np.convolve(rx_signal, rrc, "same")
+    # rx_signal_delayed = np.convolve(rx_signal, delay, 'same') # Add timing error
+    rx_signal_delayed = rx_signal
     noise = noise_component_power*np.random.randn(len(rx_signal)) + 1j * noise_component_power*np.random.randn(len(rx_signal))
-    rx_signal += noise
-    rx_signal *= np.exp(1j*(2*np.pi*doppler*np.arange(len(rx_signal))/fs_rx + phase_offset))
-    # Filter out to known ppm error before coarse estimate (lots options for a low pass filter here, nothing fancy)
-    filtered = np.convolve(rx_signal[:2000]**2, firwin(21, cutoff=f_c*35/1e6, fs=fs_rx), "same")
+    rx_signal_delayed += noise
+    rx_signal_delayed *= np.exp(1j*(2*np.pi*doppler*np.arange(len(rx_signal))/fs_rx + phase_offset))
+    # Filter out to known ppm error before coarse estimate (lots of options for a low pass filter here, nothing fancy)
+    filtered = np.convolve(rx_signal_delayed[:2000]**2, firwin(21, cutoff=f_c*35/1e6, fs=fs_rx), "same")
     coarse_offset = coarse_correction(filtered, fs_rx)
-    frequency_corrected = rx_signal*np.exp(-1j*2*np.pi*coarse_offset*np.arange(len(rx_signal))/fs_rx)
+    frequency_corrected = rx_signal_delayed*np.exp(-1j*2*np.pi*coarse_offset*np.arange(len(rx_signal_delayed))/fs_rx)
     # Would timing/matched filter would need to be acquired before a full carrier recover (prompt assumes timing).
     # timing_error_estimates = timing_recover(frequency_corrected)
     filtered_corrected = np.convolve(frequency_corrected, np.flip(rrc), "same")
+    timing_offset = 2
     timed_corrected = filtered_corrected[timing_offset::oversample]
     fine_corrected, fine_freq, fine_phase = fine_tracking(timed_corrected, oversample, fs_rx)
     check = filtered_corrected*np.exp(-1j*2*np.pi*fine_freq[-1]*np.arange(len(rx_signal))/fs_rx)
---
+++ test/timing_synch.py
@@ -0,0 +1,47 @@
+from dsp.carrier_recover import coarse_correction, fine_tracking
+from commpy import rrcosfilter
+from plotting.BER import plot_bit_error_rates
+import matplotlib.pyplot as plt
+import scipy.signal as signal
+import numpy as np
+
+num_bits = 500
+alpha = .35
+Ts = 1/1e6
+fs_rx = 4e6
+f_c = 2e9
+ebnos_db = np.linspace(0, 10, 3)
+oversample = 4
+nrz_steam = -np.random.randint(0, 2, num_bits)*2 + 1
+BER = []
+rrc = rrcosfilter(4, alpha, Ts, fs_rx)[1][1:]
+rrc /= np.linalg.norm(rrc)
+N = 21
+
+# sample_delay = np.random.uniform(0, 2)
+sample_delay = 2.5
+delay = np.sinc(np.arange(-N//2, N//2) - sample_delay)
+delay *= np.hamming(N)
+delay /= np.sum(delay)
+delay_correct = np.sinc(np.arange(-N//2, N//2) + sample_delay)
+delay_correct *= np.hamming(N)
+delay_correct /= np.sum(delay_correct)
+check = np.exp(1j*2*np.pi*np.arange(200)/20)
+check_delay = np.convolve(check, delay, 'same')
+check_delay_corrected = np.convolve(check_delay, delay_correct, 'same')
+orig_resample = signal.resample_poly(check, 4, 1)
+delay_resample = signal.resample_poly(check_delay, 4, 1)
+delay_correct_resample = signal.resample_poly(check_delay_corrected, 4, 1)
+rx_signal = 1j*np.zeros(oversample*len(nrz_steam))
+# Use random doppler in ppm tolerance range
+ppm_error = np.random.uniform(-30, 30)/1e6
+doppler = f_c*ppm_error
+phase_offset = np.random.uniform(-np.pi, np.pi)
+# Allowed to assume timing is preserved througout.
+timing_offset = 0
+rx_signal[timing_offset::oversample] = nrz_steam
+rx_signal = np.convolve(rx_signal, rrc, "same")
+rx_signal_delayed = np.convolve(rx_signal, delay, "same")
+plt.plot(rx_signal_delayed)
+plt.plot(rx_signal)
+pass
