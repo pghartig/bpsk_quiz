e47ae696d3a0c4aa8ffc7644d68a4acdac4bb05e
Peter Hartig <peterhartig@Peters-MBP-2.fios-router.home>
2022-05-02 21:06:10 -0400

    Add some comments

--- dsp/timing_recover.py
+++ dsp/timing_recover.py
@@ -14,22 +14,26 @@ def get_delayed_samples(samples, delay):
     return delayed_samples
 
 def timing_recover(signal_chunk, oversample):
+    """
+    Based on an early-late timing recovery method.
+    :param signal_chunk:
+    :param oversample:
+    :return:
+    """
     length_side = oversample - 1
     early_late_filter = np.asarray([-1, -1, -1, 0, 1, 1, 1.0])
-    # early_late_filter = np.asarray([-1, -1, -1, 0, 1, 1, 1.0]) #try complex conjugate here
-    # length_side = oversample - 2
     early_late_filter /= np.sum(np.abs(early_late_filter))
     timing_step = .005    # in units of samples
     delay_estimate = 0
     estimates = []
     check = []
     for symbol_idx in range(int(len(signal_chunk)/oversample - 2)):
-        # TODO this could be implemented more efficiently using a Farrow structure
         if symbol_idx > 0:
-            # sample_idx = oversample - 1 + symbol_idx*oversample
             sample_idx = symbol_idx*oversample
             # Could also add phase estimate into this stage for increased efficiency.
             samples_mult = signal_chunk[sample_idx-length_side: sample_idx + length_side + 1]
+            # This could be implemented more efficiently using a Farrow structure/lagrange interpolation
+            # to allow for the filter coeffients to be adjusted automatically by the delay estimate.
             delayed_samples = get_delayed_samples(samples_mult, -delay_estimate)
             left_samples = np.sum(delayed_samples[:3])
             right_samples = np.sum(delayed_samples[4:])
